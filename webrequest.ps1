c73586c06918262e79bdb206dd36035b9169801e
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2021-11-22 19:59:49 +0900

    fix

--- webrequest.ps1
+++ webrequest.ps1
@@ -1,2 +1,15 @@
-Start-Sleep 1
-Invoke-WebRequest "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/VisualStudioClient/vsextensions/MicrosoftVisualStudio2022InstallerProjects/0.1.0/vspackage" -OutFile "VSInstallerProjSetup.vsix"
+function GetInstaller {  
+    Invoke-WebRequest "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/VisualStudioClient/vsextensions/MicrosoftVisualStudio2022InstallerProjects/0.1.0/vspackage" -OutFile "VSInstallerProjSetup.vsix"   
+}
+
+for ($i=0; $i -lt 10; $i++){
+    GetInstaller
+
+    if (Test-Path "VSInstallerProjSetup.vsix") {
+        break
+    } elseif ($i -eq 9) {
+        Write-Output "Failed to get installer"
+    }
+
+    Start-Sleep 3
+}
