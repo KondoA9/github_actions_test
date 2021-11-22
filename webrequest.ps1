ab9bcc9efb054629cceb90c8f94c2d8b6c16eb95
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2021-11-22 20:01:30 +0900

    fix

--- webrequest.ps1
+++ webrequest.ps1
@@ -6,9 +6,10 @@ for ($i=0; $i -lt 10; $i++){
     GetInstaller
 
     if (Test-Path "VSInstallerProjSetup.vsix") {
-        break
+        exit 0
     } elseif ($i -eq 9) {
         Write-Output "Failed to get installer"
+        exit 1
     }
 
     Start-Sleep 3
